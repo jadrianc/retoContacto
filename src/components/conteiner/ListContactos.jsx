bc1afc02e748a6b1c9e5d73abffa8163c04fe567
jadrianc <jadriancmadrid@gmail.com>
2022-09-29 20:59:44 -0600

    se agrego boton login y logout

--- src/components/conteiner/ListContactos.jsx
+++ src/components/conteiner/ListContactos.jsx
@@ -1,12 +1,19 @@
 
+import { useState } from 'react'
 import { Contacto } from '../../module/Contacto.class'
 import ContactosComponent from '../pure/Contactos'
 const ListContactos = (props) => {
-  const defaultContacto = new Contacto('Adrian', 'Campos', 'adrian@gmail.com', true)
+  const defaultContacto = new Contacto('Adrian', 'Campos', 'adrian@gmail.com', false)
+
+  const [contacto, setContacto] = useState({ defaultContacto })
+  const onLogin = () => {
+    setContacto({ ...contacto, connect: !contacto.connect })
+  }
   return (
     <>
       <h2>Lista de contactos</h2>
-      <ContactosComponent contacto={defaultContacto}> </ContactosComponent>
+      <ContactosComponent contacto={contacto}> </ContactosComponent>
+      <button onClick={onLogin}> {contacto.connect ? 'Logout' : 'login'} </button>
     </>
   )
 }
